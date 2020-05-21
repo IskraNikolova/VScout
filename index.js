6cd60739197d0cfcdb80da0a6515c6137d60a2e1
IskraNikolova <air_iskra@yahoo.com>
2020-05-21 14:48:52 +0300

    fix for heroku

---
+++ index.js
@@ -0,0 +1,19 @@
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url)
+  } else {
+    next()
+  }
+})
+
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa'))
+app.listen(port)
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="page-cont">
-    <!--<q-img src="~assets/backgrounds/5.png" class="fullscreen" style="z-index: -1;width: 55%;"/>-->
     <q-page-container>
       <router-view />
     </q-page-container>
