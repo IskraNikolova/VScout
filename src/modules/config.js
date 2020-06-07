767546cfaf0f726c41ddd733ab1bfcf11362af67
IskraNikolova <air_iskra@yahoo.com>
2020-06-07 20:39:25 +0300

    added endpoint

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,6 +1,6 @@
 export default {
   network: {
-    endpointUrls: ['https://bootstrap.ava.network:21000/', 'http://127.0.0.1:9650/'],
+    endpointUrls: ['https://bootstrap.ava.network:21000/', 'https://explorerapi.ava.network:21000/', 'http://127.0.0.1:9650/'],
     explorerApiBaseUrl: 'https://explorerapi.ava.network/',
     contract: '0x3408c96c2448f1cc1934668aD677BAa028d06e04',
     endpointCChain: 'bootstrap.ava.network:21000/ext/C/ws',
