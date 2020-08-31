ab5cd31c7a09857f2eac09020e6a2ae6e154122b
IskraNikolova <air_iskra@yahoo.com>
2020-08-31 13:31:31 +0300

    new icons and error panel design

--- src/components/panels/error.vue
+++ src/components/panels/error.vue
@@ -1,11 +1,13 @@
 <template>
   <q-card
+    flat
     v-if="!healthy"
-    class="q-mt-md q-pt-md q-pl-xl q-pb-md"
-    id="custom-card"
+    class="q-mt-xs"
+    id="custom-card-2"
+    align="right"
   >
-    <span class="text-negative" v-if="error">{{ error }} <q-spinner-dots  color="negative" size="1em"/></span>
-    <span class="text-negative" v-else>Connecting <q-spinner-dots  color="negative" size="1em"/></span>
+    <span class="text-orange" v-if="error"><img src="~assets/warning.svg" id="logo"> {{ error }} <q-spinner-dots  color="orange" size="1em"/></span>
+    <span class="text-orange" v-else><img src="~assets/warning.svg" id="logo"> Connecting <q-spinner-dots  color="orange" size="1em"/></span>
   </q-card>
 </template>
 
@@ -43,3 +45,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ #custom-card-2 {
+   background-image: linear-gradient(45deg, #32353b 41.67%, #1c2a33 41.67%, #1c2a33 50%, #32353b 50%, #32353b 91.67%, #1c2a33 91.67%, #1c2a33 100%);
+   background-size: 8.49px 8.49px;
+   font-size: 17px;
+ }
+  #logo {
+    width:30vw;
+    max-width:30px;
+    margin-bottom: -4px;
+  }
+</style>
