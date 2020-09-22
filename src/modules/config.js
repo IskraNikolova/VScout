15aeaffb3d7d2b6beff8640300c9577d57e64632
mrFawkes <45701628+mrFawkes@users.noreply.github.com>
2020-09-22 05:01:05 +0300

    Update config.js

--- src/modules/config.js
+++ src/modules/config.js
@@ -7,7 +7,7 @@ export default {
     port: 443,
     explorerApiBaseUrl: 'https://explorerapi.avax.network/',
     contract: '0x209D694B5c46B36CBf287BB43cDc999410A2c766',
-    endpointCChain: 'testapi.avax.network/ext/bc/C/rpc',
+    endpointCChain: 'api.avax.network/ext/bc/C/rpc',
     address: '0x4aBeF613822Fb2031D897E792f89C896dDaFC466',
     defaultSubnetID: '11111111111111111111111111111111LpoYY',
     endpointUrls: [
