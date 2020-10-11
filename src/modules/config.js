dd2ff2e0a2f2d088a2864034399c42437e09b1b8
IskraNikolova <air_iskra@yahoo.com>
2020-10-11 22:56:36 +0300

    fixed url

--- src/modules/config.js
+++ src/modules/config.js
@@ -6,7 +6,7 @@ export default {
     chainId: 'X',
     port: 443,
     cChainId: 43114,
-    explorerApiBaseUrl: 'https://explorerapi.avax.network/',
+    explorerApiBaseUrl: 'https://explorerapi.avax-dev.network/',
     contract: '0x209D694B5c46B36CBf287BB43cDc999410A2c766',
     endpointCChain: 'api.avax.network/ext/bc/C/rpc',
     address: '0x4aBeF613822Fb2031D897E792f89C896dDaFC466',
