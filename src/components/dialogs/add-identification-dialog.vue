fa3d4a8684faa830844cc7c58e71ada12dae9f48
IskraNikolova <air_iskra@yahoo.com>
2020-10-20 19:34:20 +0300

    change endpoint

--- src/components/dialogs/add-identification-dialog.vue
+++ src/components/dialogs/add-identification-dialog.vue
@@ -291,7 +291,7 @@ export default {
           return
         }
         const minutes = getDurationByMinutesCount(timestamp)
-        if (minutes > 230) {
+        if (minutes > 30) {
           this.onFailed('Verification Transaction Failed! Expired Transaction.')
           return
         }
