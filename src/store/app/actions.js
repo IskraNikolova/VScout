aea063126c52219888bba88f42c97cbb30998f56
IskraNikolova <air_iskra@yahoo.com>
2020-10-22 17:01:17 +0300

    fixed rpc

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -78,13 +78,13 @@ async function initApp (
       dispatch(GET_NODE_INFO),
       dispatch(GET_NODE_HEALTH),
       dispatch(GET_ASSETS_BY_BLOCKCHAINS),
+      dispatch(GET_HEIGHT, {}),
+      dispatch(GET_CURRENT_SUPPLY),
       _initializeNetwork(),
       dispatch(GET_STAKING, {
         subnetID: getters.subnetID,
         isInit: true
       }),
-      dispatch(GET_HEIGHT, {}),
-      dispatch(GET_CURRENT_SUPPLY),
       dispatch(GET_PENDING_STAKING, {})
     ])
   } catch (err) {
