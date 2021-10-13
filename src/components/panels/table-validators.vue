8da1b4aee0ca6c5eca8d44dbe0b459ddbeff89fe
IskraNikolova <air_iskra@yahoo.com>
2021-10-13 14:20:54 +0300

    Fixed uptimes colors

--- src/components/panels/table-validators.vue
+++ src/components/panels/table-validators.vue
@@ -819,7 +819,8 @@ export default {
       return round(Number(val), 1000)
     },
     getColorUptime (val) {
-      if (val >= 0.6) return 'positive'
+      if (val >= 0.83) return 'positive'
+      else if (val >= 0.8) return 'warning'
       return 'negative'
     },
     getFormatReward (val) {
