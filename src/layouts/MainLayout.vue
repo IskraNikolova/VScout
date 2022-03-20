b21bbe88a0868fbcb2b5b6e043cdc0e03b442cf0
IskraNikolova <air_iskra@yahoo.com>
2022-03-20 09:30:55 +0200

    fixed countdown

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -696,12 +696,12 @@ export default {
       return this.currenciesPriceList[`${this.currentCurrency}`]
     },
     isValidatorShow (id) {
-      if (!id) return
-      const isVal = this.validatorById(id)
-      if (isVal) {
-        return id !== 'NodeID-2KfgS6P7vf9L55fMRTbHPgS4ugVSDW3nj'
-      }
-      return false
+      // if (!id) return
+      // const isVal = this.validatorById(id)
+      // if (isVal) {
+      //  return id !== 'NodeID-2KfgS6P7vf9L55fMRTbHPgS4ugVSDW3nj'
+      // }
+      // return false
     },
     switchTheme (theme) {
       if (!theme) return
