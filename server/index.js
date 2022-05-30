6aa3c7e6826b9eb140e9af23c4f4fc29eb84ceb2
IskraNikolova <air_iskra@yahoo.com>
2022-05-30 20:19:25 +0300

    tested get observers function

--- server/index.js
+++ server/index.js
@@ -14,26 +14,26 @@ require('./config/routes')(app)
 app.listen(config.port, () => console.log(`Server start on port ${config.port}`))
 
 // const endpoint = 'http://135.181.144.201:9650'
-// const endpoint =  'http://165.173.18.156:9650'
+const endpoint =  'http://165.173.18.156:9650'
 
-let endpoints = getEndpoints()
+// let endpoints = getEndpoints()
 
 let i = 0
 setInterval(() => {
-  const endpoint = endpoints[i]
+  // const endpoint = endpoints[i]
   controllers.avax.avaxPrice()
   controllers.platform.blockHeight(endpoint)
   controllers.validators.validators(endpoint)
   controllers.node.info(endpoint)
   controllers.node.peersPost(endpoint)
 
-  if (i >= endpoints.length) {
-    endpoints = getEndpoints()
-    i = 0
-  } else {
-    i++
-  }
-}, 30000)
+  // if (i >= endpoints.length) {
+  //   endpoints = getEndpoints()
+  //   i = 0
+  // } else {
+  //   i++
+  // }
+}, 3000)
 
 let index = 500
 let inProcess = false
@@ -63,4 +63,4 @@ setInterval(() => {
   } catch (err) {
     console.log(err)
   }
-}, 100000)
+}, 1000)
